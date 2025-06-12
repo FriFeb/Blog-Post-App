440c7960e3ebe9a64ab208539bcedd24b8b8fa97
Fedir Denysenko <51440204+FriFeb@users.noreply.github.com>
2025-06-12 20:44:42 +0300

    setup DB

--- config/keys.js
+++ config/keys.js
@@ -1,58 +1,67 @@
-
-const mongoURI = 'mongodb://localhost/blogs';
+const mongoURI =
+  'mongodb+srv://feddenisenko1:6tCdtqgmCkltLjOu@blogs.5saiewo.mongodb.net/';
 
 const smtpConfig = {
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // use SSL
-    auth: {
-        user: 'xyz@xyz.com', // Change it
-        pass: 'yourpwd' // Change it
-    }
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // use SSL
+  auth: {
+    user: '1', // Change it
+    pass: '2', // Change it
+  },
 };
 
 // Registration
 const regMailOptions = {
-    from: 'ashis.raj@gmail.com',
-    subject: 'Welcome to Node.js Blog App - Registration Successful!',
-    text: 'Congratulations for the successful registration to our web application!',
-    html: '<b>Congratulations for the successful registration to our web application!<b>'
+  from: 'xyz@xyz.com', // Change it
+  subject: 'Welcome to Node.js Blog App - Registration Successful!',
+  text: 'Congratulations for the successful registration to our web application!',
+  html: '<b>Congratulations for the successful registration to our web application!<b>',
 };
 
 // Forgot Password
 const forgotMailOptions = {
-        from: 'xyz@xyz.com', // Change it
-        subject: 'Node.js Blog App - Password Reset',
-        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'http://<host>/reset/<token>' + '\n\n' +
-          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+  from: 'xyz@xyz.com', // Change it
+  subject: 'Node.js Blog App - Password Reset',
+  text:
+    'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+    'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+    'http://<host>/reset/<token>' +
+    '\n\n' +
+    'If you did not request this, please ignore this email and your password will remain unchanged.\n',
 };
 
 // Reset Password
 var resetMailOptions = {
-        from: 'xyz@xyz.com', // Change it
-        subject: 'Node.js Blog App - Your password has been changed',
-        text: 'Hello,\n\n' + 
-          'This is a confirmation that the password for your account <email> has just been changed.\n'
+  from: 'xyz@xyz.com', // Change it
+  subject: 'Node.js Blog App - Your password has been changed',
+  text:
+    'Hello,\n\n' +
+    'This is a confirmation that the password for your account <email> has just been changed.\n',
 };
 
 // Blog/Post creation
 
 // Add new key, value pair to the existing object
 const addKeyValue = (obj, key, value) => {
-    obj[key] = value;
-    return obj;
+  obj[key] = value;
+  return obj;
 };
 
-const passwordExpirationTimeInMills = (60 * 60 * 1000); // 1 hour
+const passwordExpirationTimeInMills = 60 * 60 * 1000; // 1 hour
 
-const cookieMaxAge = (10 * 60 * 1000)	// 10 minutes
+const cookieMaxAge = 10 * 60 * 1000; // 10 minutes
 
-const disableEmailSending = "yes";
+const disableEmailSending = 'yes';
 
 module.exports = {
-    mongoURI, smtpConfig, regMailOptions, forgotMailOptions,
-    resetMailOptions, passwordExpirationTimeInMills, cookieMaxAge,
-    disableEmailSending, addKeyValue
-}
+  mongoURI,
+  smtpConfig,
+  regMailOptions,
+  forgotMailOptions,
+  resetMailOptions,
+  passwordExpirationTimeInMills,
+  cookieMaxAge,
+  disableEmailSending,
+  addKeyValue,
+};
